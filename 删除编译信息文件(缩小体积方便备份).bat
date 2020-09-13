e70ff60309d55a6cdbd036a6855373f54e6dc403
cctv180 <cctv180@gmail.com>
2020-09-13 20:54:03 +0800

    启用GIT托管

---
+++ 删除编译信息文件(缩小体积方便备份).bat
@@ -0,0 +1,45 @@
+rd /Q /S MDK-ARM\Objects
+rd /Q /S MDK-ARM\Listings
+rd /Q /S MDK-ARM\DebugConfig
+rd /Q /S MDK-ARM\SD_J_B_stm32f4
+rd /Q /S MDK-ARM\project
+
+del /Q MDK-ARM\*.bak
+del /Q MDK-ARM\*.dep
+del /Q MDK-ARM\*.uvguix.*
+del /Q MDK-ARM\*.scvd
+#del /Q MDK-ARM\JLink*
+del /Q MDK-ARM\JLinkLog.txt
+del /Q MDK-ARM\project.uvgui.*
+
+rd  /Q /S EmbeddedProject1\VisualGDB
+rd  /Q /S EmbeddedProject1\.vs
+del /Q EmbeddedProject1\*.vcxproj.user
+del /Q EmbeddedProject1\*.vgdbsettings
+
+#copy EWARM\SD_J_B_stm32f4\Exe\*.hex SD-J-B_V2.01.hex
+
+rd /Q /S EWARM\bin
+rd /Q /S EWARM\obj
+rd /Q /S EWARM\settings
+rd /Q /S EWARM\SD_J_B_stm32f4
+rd /Q /S EWARM\.vs
+
+del /Q   EWARM\settings\*.crun
+del /Q   EWARM\settings\*.dnx
+del /Q   EWARM\settings\*.cspy
+del /Q   EWARM\settings\*.general.xcl
+del /Q   EWARM\settings\*.wsdt
+del /Q   EWARM\settings\*.cspy.bat
+del /Q   EWARM\settings\*.dbgdt
+del /Q   EWARM\settings\*.ps1
+del /Q   EWARM\SD_J_B_stm32f4.dep
+del /Q   EWARM\*.sln
+del /Q   EWARM\*.vcxproj
+del /Q   EWARM\*.filters
+del /Q   EWARM\*.opendb
+del /Q   EWARM\*VC.db
+del /Q   EWARM\IAR_Gen.lua
+del /Q   EWARM\*.user
+
+exit
